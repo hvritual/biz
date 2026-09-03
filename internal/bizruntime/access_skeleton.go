290732cfc2ac0202182bb0c241bf7cdf6ec140aa
hvritual <460657446@qq.com>
2026-09-03 20:46:48 +0800

    feat(b13): compose tenant delegation application at runtime

--- internal/bizruntime/access_skeleton.go
+++ internal/bizruntime/access_skeleton.go
@@ -7,6 +7,16 @@ import (
 	generatedassembly "github.com/hvritual/biz/internal/assembly"
 )
 
+func (factory applicationFactories) BuildAccessTenantDelegationManagement(dependencies generatedassembly.AccessTenantDelegationManagementDependencies) (accessapp.TenantDelegationManagementApplication, error) {
+	if dependencies.AccessTenantLifecycle == nil || dependencies.DeviceopsDeviceManagement == nil {
+		return nil, errors.New("biz access pressure: tenant delegation dependencies are required")
+	}
+	return accessapp.NewTenantDelegationManagementService(factory.delegationRepositories, tenantDelegationManagementCapabilities{
+		tenants: dependencies.AccessTenantLifecycle,
+		devices: dependencies.DeviceopsDeviceManagement,
+	})
+}
+
 func (factory applicationFactories) BuildAccessTenantMemberLifecycle(dependencies generatedassembly.AccessTenantMemberLifecycleDependencies) (accessapp.TenantMemberLifecycleApplication, error) {
 	if dependencies.AccessTenantRolePermission == nil {
 		return nil, errors.New("biz access pressure: tenant member lifecycle role dependency is required")
@@ -28,6 +38,19 @@ func (factory applicationFactories) BuildAccessTenantLifecycle(dependencies gene
 		members: dependencies.AccessTenantMemberLifecycle,
 		roles: dependencies.AccessTenantRolePermission,
 	})
+}
+
+type tenantDelegationManagementCapabilities struct {
+	tenants accessapp.TenantDelegationManagementToAccessTenantLifecycleChildCapability
+	devices accessapp.TenantDelegationManagementToDeviceopsDeviceManagementChildCapability
+}
+
+func (capabilities tenantDelegationManagementCapabilities) AccessTenantLifecycle() accessapp.TenantDelegationManagementToAccessTenantLifecycleChildCapability {
+	return capabilities.tenants
+}
+
+func (capabilities tenantDelegationManagementCapabilities) DeviceopsDeviceManagement() accessapp.TenantDelegationManagementToDeviceopsDeviceManagementChildCapability {
+	return capabilities.devices
 }
 
 type tenantMemberLifecycleCapabilities struct {
