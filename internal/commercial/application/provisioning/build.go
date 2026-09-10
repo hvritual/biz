dbaef64e5ab8e5265492a1e8df3d6e8842cd3ebf
hvritual <460657446@qq.com>
2026-09-10 23:33:43 +0800

    feat(ce-10): construct provisioning application behind declared interfaces

---
+++ internal/commercial/application/provisioning/build.go
@@ -0,0 +1,9 @@
+package provisioning
+
+import (
+ app "github.com/hvritual/biz/internal/commercial/application"
+ "github.com/hvritual/biz/internal/commercial/application/provisioning/internal/usecase"
+ "github.com/hvritual/biz/internal/commercial/ports"
+ "yunka.io/framework/requestscope"
+)
+func Build(r requestscope.RepositoryFactory[ports.ProvisioningRepositories],c app.ProvisioningCapabilities)(app.ProvisioningApplication,error){return usecase.New(r,c)}
