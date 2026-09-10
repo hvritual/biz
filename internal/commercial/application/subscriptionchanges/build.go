3842073c605c21ed3c7a474be8202659332467c9
hvritual <460657446@qq.com>
2026-09-10 19:34:29 +0800

    feat(ce-09): assemble subscription changes through declared capabilities

---
+++ internal/commercial/application/subscriptionchanges/build.go
@@ -0,0 +1,9 @@
+package subscriptionchanges
+
+import (
+ app "github.com/hvritual/biz/internal/commercial/application"
+ "github.com/hvritual/biz/internal/commercial/application/subscriptionchanges/internal/usecase"
+ "github.com/hvritual/biz/internal/commercial/ports"
+ "yunka.io/framework/requestscope"
+)
+func Build(r requestscope.RepositoryFactory[ports.SubscriptionChangeRepositories],c app.SubscriptionChangesCapabilities,s ports.EntitlementSnapshotReader,q ports.QuotaChangePolicy)(app.SubscriptionChangesApplication,error){return usecase.New(r,c,s,q)}
