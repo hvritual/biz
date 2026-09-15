674ba9481d4b24d7a3b0e4d99a4cbf72ecf25bb8
hvritual <460657446@qq.com>
2026-09-15 11:42:09 +0800

    feat(web): split real and demo plan surfaces

---
+++ web/src/features/enterprise/pages/PlansEntryView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PlansRealView from './PlansRealView.vue'
+import PlansView from './PlansView.vue'
+
+const realMode = (import.meta.env.VITE_DATA_MODE ?? 'demo') === 'api'
+</script>
+
+<template>
+  <PlansRealView v-if="realMode" />
+  <PlansView v-else />
+</template>
